caf7f336d09b7ef974d31dd4280d66f75895af05
LukeJ <LukeRJohnsonUT@gmail.com>
2023-03-11 10:59:25 -0700

    updated main js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login(){
   const nameEl = document.querySelector("#name");
-  localStorage.setItem('userName', nameEl);
+  localStorage.setItem('userName', nameEl.value);
   window.location.href= 'home.html';
 }
